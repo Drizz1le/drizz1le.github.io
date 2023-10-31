ee95bbb94240cb3458a07e39e5c98f83f18d2ede
Ben <bkudarauskas@gmail.com>
2023-10-31 18:35:50 -0400

    update

--- js/config.js
+++ js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "Drizzle's Shop",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
